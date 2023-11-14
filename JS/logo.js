ac3cd4ea34d1247a68d22b4fbaff2f43d1a81664
Lau Ra <newam2s.lau@gmail.com>
2023-11-14 17:48:29 +0100

    add pictures, scss and 1st js files

---
+++ JS/logo.js
@@ -0,0 +1,32 @@
+
+const logoHeader= document.querySelector(".logoHeader--Logo");
+const titleHeader= document.querySelector(".logoHeader--Title");
+logoHeader.style.display="none";
+titleHeader.style.display="none"
+const logoPage=document.querySelector('.logoHome');
+
+let positionLogoPage= logoPage.getBoundingClientRect();
+let logoY=positionLogoPage.bottom;
+
+let positionScroll = 0;
+
+/*----------------------FONCTIONS---------------------------*/
+
+
+function logoMvmnt(){
+    positionScroll=window.pageYOffset;
+    if(logoY>positionScroll){
+        logoHeader.style.display="none";
+        titleHeader.style.display="none"
+        logoPage.style.display="flex";
+
+    }
+    else if(logoY<positionScroll){
+        logoHeader.style.display="flex";
+        titleHeader.style.display="block"
+        logoPage.style.display="none";
+    }
+}
+
+
+window.addEventListener('scroll',logoMvmnt);//transition du logo HomePage vers Logo bannière
